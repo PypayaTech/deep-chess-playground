475494462fa2e276dbc63e011387fb109af77447
PypayaTech <138917152+PypayaTech@users.noreply.github.com>
2023-07-08 12:23:16 +0200

    Add some data processing and modelling code in the two_d_cnn package.

---
+++ src/cnn/two_d_cnn/modelling/backbones.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+from src.cnn.two_d_cnn.position_evaluation_and_move_classification.modelling.simple_modules import ConvolutionalBlock, ResidualBlock
+
+
+class ConvolutionalTower:
+    def __init__(self, input_planes, num_blocks, channels):
+        super().__init__(channels, channels)
+        self.blocks = nn.ModuleList()
+        self.blocks.append(ConvolutionalBlock(input_planes, channels))
+        for _ in range(num_blocks - 1):
+            self.blocks.append(ConvolutionalBlock(channels, channels))
+
+    def forward(self, x):
+        for block in self.blocks:
+            x = block(x)
+        return x
+
+
+class ResidualTower:
+    def __init__(self, input_planes, num_blocks, channels):
+        super().__init__(channels, channels)
+        self.blocks = nn.ModuleList()
+        self.blocks.append(ResidualBlock(input_planes, channels))
+        for _ in range(num_blocks - 1):
+            self.blocks.append(ResidualBlock(channels, channels))
+
+    def forward(self, x):
+        for block in self.blocks:
+            x = block(x)
+        return x
